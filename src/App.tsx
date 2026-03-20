a895c277e99c6666caebc71114a05e9749953496
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-21 00:14:39 +0500

    updated the navbar also added the top scroll one the changing of routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import SoftBackdrop from './components/SoftBackdrop';
-import Footer from './components/Footer';
-import LenisScroll from './components/lenis';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SoftBackdrop from "./components/SoftBackdrop";
+import Footer from "./components/Footer";
+import LenisScroll from "./components/lenis";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
-	return (
-		<>
-			<SoftBackdrop />
-			<LenisScroll />
-			<Navbar />
-			<Home />
-			<Footer />
-		</>
-	);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return (
+    <>
+      <SoftBackdrop />
+      <LenisScroll />
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 export default App;
