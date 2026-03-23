b3dd1a8d10b0148b871a05830a83b399041adf86
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-23 22:43:21 +0500

    configured the clerk for authentication also settup the links/routes for the signin and signup component to be opened.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+import { dark } from "@clerk/themes";
 
-createRoot(document.getElementById('root')! as HTMLElement).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+const phublishable_Key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!phublishable_Key) {
+  throw new Error("Key not found!");
+}
+
+createRoot(document.getElementById("root")! as HTMLElement).render(
+  <BrowserRouter>
+    <ClerkProvider
+      publishableKey={phublishable_Key}
+      appearance={{
+        theme: dark,
+        variables: {
+          colorPrimary: "#4f39f6",
+          colorTextOnPrimaryBackground: "#ffffff",
+        },
+      }}
+    >
+      <App />
+    </ClerkProvider>
+  </BrowserRouter>,
+);
