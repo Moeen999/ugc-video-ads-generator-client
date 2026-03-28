150593fab15ff6d4c31baf4432816ff906ba166a
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-28 12:04:02 +0500

    implemented the logic to get the user credits and also added the logic of video generation and integrated the api (60%).

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,25 @@ import {
   DollarSignIcon,
   FolderEditIcon,
   GalleryHorizontalEnd,
+  Loader2Icon,
   MenuIcon,
   SparkleIcon,
   XIcon,
 } from "lucide-react";
 import { GhostButton, PrimaryButton } from "./Buttons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useClerk, UserButton, useUser } from "@clerk/react";
+import { useAuth, useClerk, UserButton, useUser } from "@clerk/react";
+import api from "../config/axios";
+import toast from "react-hot-toast";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [credits, setCredits] = useState(0);
+  const { pathname } = useLocation();
+  const { getToken } = useAuth();
   const { user } = useUser();
   const { openSignIn, openSignUp } = useClerk();
   const navigate = useNavigate();
@@ -26,6 +32,28 @@ export default function Navbar() {
     { name: "Plans", href: "/plans" },
   ];
 
+  const getUserCredits = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await api.get("/api/user/credits", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCredits(data?.credits);
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      (async () => {
+        await getUserCredits();
+      })();
+    }
+  }, [user, pathname]);
   return (
     <motion.nav
       className="fixed top-5 left-0 right-0 z-50 px-4"
@@ -72,7 +100,7 @@ export default function Navbar() {
               onClick={() => navigate("/plans")}
               className="border-none text-gray-300 sm:py-1.5"
             >
-              Credits:
+              Credits: {credits}
             </GhostButton>
             <UserButton>
               <UserButton.MenuItems>
